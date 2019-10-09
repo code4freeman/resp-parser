a93abf6688d527d9f6f4403f1c1fba73048bd4d5
lilindog <lilin@lilin.site>
2019-10-09 17:30:38 +0800

    下班提交

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 "use strict"
 
 const {EventEmitter:Emitter} = require("events");
-let test = 0;
+
 module.exports = class Parse extends Emitter{
 
 	constructor () {
 		super();
+		/**
+		 * debug模式 
+		 */
 		this.DEBUG = true;
+		/**
+		 * 是否缺失
+		 * 用作深度递归里边遇到resp长度不够的时候停止外层
+		 */
+		this.isLack = false;
 		/**
 		 * 返回数据累计 
 		 */
@@ -64,31 +72,34 @@ module.exports = class Parse extends Emitter{
 		 */
 		if (this.chunk[this.index] === this.ascii.ADD) {
 			this.index ++;
-			let data = [], byte = null;
+			let data = [], byte = null, isBreak = false;
 			while (this.index < this.chunk.byteLength) {
 				byte = this.chunk[this.index];
 				if (byte === this.ascii.CR && this.chunk[this.index + 1] === this.ascii.LF) {
+					isBreak = true;
 					break;
 				} else {
 					data.push(byte);
 					this.index ++;
 				}
 			}
-			/**
-			 * 在递归处理*时候与正常解析的处理方式不一样的 
-			 */
-			if (this.deepStack.length > 0) {
-				data = Buffer.from(data).toString();
-				const lastChild = this.deepStack[this.deepStack.length - 1];
-				lastChild.num ++;
-				lastChild.data.push(data);
-				this.index += 2;
-				if (lastChild.num === lastChild.length) return;
-			} else {
-				data = Buffer.from(data).toString();
-				this.emit("data", data);
-				this.chunk = this.chunk.slice(this.index + 2);
-				this.index = 0;
+			if (isBreak) {
+				/**
+				 * 在递归处理*时候与正常解析的处理方式不一样的 
+				 */
+				if (this.deepStack.length > 0) {
+					data = Buffer.from(data).toString();
+					const lastChild = this.deepStack[this.deepStack.length - 1];
+					lastChild.num ++;
+					lastChild.data.push(data);
+					this.index += 2;
+					if (lastChild.num === lastChild.length) return;
+				} else {
+					data = Buffer.from(data).toString();
+					this.emit("data", data);
+					this.chunk = this.chunk.slice(this.index + 2);
+					this.index = 0;
+				}
 			}
 		}
 
@@ -97,28 +108,31 @@ module.exports = class Parse extends Emitter{
 		 */
 		if (this.chunk[this.index] === this.ascii.SUB) {
 			this.index ++;
-			let data = [], byte = null;
+			let data = [], byte = null, isBreak = false;
 			while (this.index < this.chunk.byteLength) {
 				byte = this.chunk[this.index];
 				if (byte === this.ascii.CR && this.chunk[this.index + 1] === this.ascii.LF) {
+					isBreak = true;
 					break;
 				} else {
 					data.push(byte);
 					this.index ++;
 				}
 			}
-			if (this.deepStack.length > 0) {
-				data = Buffer.from(data).toString();
-				const lastChild = this.deepStack[this.deepStack.length - 1];
-				lastChild.num ++;
-				lastChild.data.push(data);
-				this.index += 2;
-				if (lastChild.num === lastChild.length) return;
-			} else {
-				data = Buffer.from(data).toString();
-				this.emit("data", data);
-				this.chunk = this.chunk.slice(this.index + 2);
-				this.index = 0;
+			if (isBreak) {
+				if (this.deepStack.length > 0) {
+					data = Buffer.from(data).toString();
+					const lastChild = this.deepStack[this.deepStack.length - 1];
+					lastChild.num ++;
+					lastChild.data.push(data);
+					this.index += 2;
+					if (lastChild.num === lastChild.length) return;
+				} else {
+					data = Buffer.from(data).toString();
+					this.emit("data", data);
+					this.chunk = this.chunk.slice(this.index + 2);
+					this.index = 0;
+				}
 			}
 		}
 
@@ -127,28 +141,31 @@ module.exports = class Parse extends Emitter{
 		 */
 		if (this.chunk[this.index] === this.ascii.COLON) {
 			this.index ++;
-			let data = [], byte = null;
+			let data = [], byte = null, isBreak = false;
 			while (this.index < this.chunk.byteLength) {
 				byte = this.chunk[this.index];
 				if (byte === this.ascii.CR && this.chunk[this.index + 1] === this.ascii.LF) {
+					isBreak = true;
 					break;
 				} else {
 					data.push(byte);
 					this.index ++;
 				}
 			}
-			if (this.deepStack.length > 0) {
-				data = Buffer.from(data).toString();
-				const lastChild = this.deepStack[this.deepStack.length - 1];
-				lastChild.num ++;
-				lastChild.data.push(data);
-				this.index += 2;
-				if (lastChild.num === lastChild.length) return;
-			} else {
-				data = Buffer.from(data).toString();
-				this.emit("data", data);
-				this.chunk = this.chunk.slice(this.index + 2);
-				this.index = 0;
+			if (isBreak) {
+				if (this.deepStack.length > 0) {
+					data = Buffer.from(data).toString();
+					const lastChild = this.deepStack[this.deepStack.length - 1];
+					lastChild.num ++;
+					lastChild.data.push(data);
+					this.index += 2;
+					if (lastChild.num === lastChild.length) return;
+				} else {
+					data = Buffer.from(data).toString();
+					this.emit("data", data);
+					this.chunk = this.chunk.slice(this.index + 2);
+					this.index = 0;
+				}
 			}
 		}
 
@@ -157,50 +174,49 @@ module.exports = class Parse extends Emitter{
 		 */
 		if (this.chunk[this.index] === this.ascii.DOLLAR) {
 			this.index ++;
-			let data = [], byte = null, length = [], num = 0;
+			let data = [], byte = null, length = [], num = 0, isBreak = false;
 			while (this.index < this.chunk.byteLength) {
 				byte = this.chunk[this.index];
 				if (byte === this.ascii.CR && this.chunk[this.index + 1] === this.ascii.LF) {
 					length = Number(Buffer.from(length).toString());
+					isBreak = true;
 					break;
 				} else {
 					length.push(byte);
 					this.index ++;
 				}
 			}
-			this.index += 2;
-			while (num <= length) {
-				byte = this.chunk[this.index + num];
-				if (!byte) {
-					this.DEBUG && console.log("$解析时候，目前chunk长度不够length，需要等待下次parse调用累积处理");
-					return;
-				}
-				if (num === length) {
-					if (byte === this.ascii.CR && this.chunk[this.index + num + 1] === this.ascii.LF) {
-						break;
+			if (isBreak) {
+				this.index += 2;
+				let isBreak1 = false;
+				while (num <= length) {
+					byte = this.chunk[this.index + num];
+					if (num === length) {
+						if (byte === this.ascii.CR && this.chunk[this.index + num + 1] === this.ascii.LF) {
+							isBreak1 = true;
+							break;
+						}
 					} else {
-						const err = new Error("解析$时候，对应数据长度不匹配, 实际数据比给定长度超出\n" + this._getErrorPositionStr());
-						this.emit("error", err);
-						throw err;
+						data.push(byte);
+						num ++;
 					}
-				} else {
-					data.push(byte);
-					num ++;
 				}
-			}
-			if (this.deepStack.length > 0) {
-				//在递归解析*时候的逻辑
-				data = Buffer.from(data).toString();
-				const lastChild = this.deepStack[this.deepStack.length - 1];
-				lastChild.num ++;
-				lastChild.data.push(data);
-				this.index = this.index + num + 2;
-				if (lastChild.num === lastChild.length) return;
-			} else {
-				data = Buffer.from(data).toString();
-				this.emit("data", data);
-				this.chunk = this.chunk.slice(this.index + num + 2);
-				this.index = 0;
+				if (isBreak1) {
+					if (this.deepStack.length > 0) {
+						//在递归解析*时候的逻辑
+						data = Buffer.from(data).toString();
+						const lastChild = this.deepStack[this.deepStack.length - 1];
+						lastChild.num ++;
+						lastChild.data.push(data);
+						this.index = this.index + num + 2;
+						if (lastChild.num === lastChild.length) return;
+					} else {
+						data = Buffer.from(data).toString();
+						this.emit("data", data);
+						this.chunk = this.chunk.slice(this.index + num + 2);
+						this.index = 0;
+					}
+				}
 			}
 		}
 
@@ -209,45 +225,48 @@ module.exports = class Parse extends Emitter{
 		 */
 		if (this.chunk[this.index] === this.ascii.STAR) {
 			this.index ++;
-			let length = 0, bytes = [], byte = null;
+			let length = 0, bytes = [], byte = null, isBreak = false;
 			while (this.index < this.chunk.byteLength) {
 				byte = this.chunk[this.index];
 				if (byte === this.ascii.CR && this.chunk[this.index + 1] === this.ascii.LF) {
 					length = Number(Buffer.from(bytes).toString());
 					this.index += 2;
+					isBreak = true;
 					break;
 				} else {
 					bytes.push(byte);
 					this.index ++;
 				}
 			}
-			//压栈、递归
-			this.deepStack.push(this._buildDeepStackChild(length));
-			this.parse();
-			const lastChild = this.deepStack[this.deepStack.length - 1];
-			if (lastChild.num === lastChild.length) {
-				/**
-				 * 深度栈有元素，代表当前操作处于*递归当中
-				 * 把当前栈顶的数据压入到倒数第二个栈元素当中；并将栈顶元素出栈
-				 * 然后return出去
-				 */
-				if (this.deepStack.length > 1) {
-					const last2Child = this.deepStack[this.deepStack.length - 2];
-					last2Child.num ++;
-					last2Child.data.push(lastChild.data);
-					this.deepStack.pop();
-					return;
-				} 
-				/**
-				 * 深度栈无元素，代表当前处于*递归完成返回
-				 * 获取数据，触发事件传出数据
-				 * 然后继续处理其他数据
-				 */
-				else {
-					this.emit("data", this.deepStack[0].data);
-					this.deepStack = [];
-					this.chunk = this.chunk.slice(this.index);
-					this.index = 0;
+			if (isBreak) {
+				//压栈、递归
+				this.deepStack.push(this._buildDeepStackChild(length));
+				this.parse();
+				const lastChild = this.deepStack[this.deepStack.length - 1];
+				if (lastChild.num === lastChild.length) {
+					/**
+					 * 深度栈有元素，代表当前操作处于*递归当中
+					 * 把当前栈顶的数据压入到倒数第二个栈元素当中；并将栈顶元素出栈
+					 * 然后return出去
+					 */
+					if (this.deepStack.length > 1) {
+						const last2Child = this.deepStack[this.deepStack.length - 2];
+						last2Child.num ++;
+						last2Child.data.push(lastChild.data);
+						this.deepStack.pop();
+						return;
+					} 
+					/**
+					 * 深度栈无元素，代表当前处于*递归完成返回
+					 * 获取数据，触发事件传出数据
+					 * 然后继续处理其他数据
+					 */
+					else {
+						this.emit("data", this.deepStack[0].data);
+						this.deepStack = [];
+						this.chunk = this.chunk.slice(this.index);
+						this.index = 0;
+					}
 				}
 			}
 		}
@@ -256,12 +275,24 @@ module.exports = class Parse extends Emitter{
 		 * 末尾检测
 		 */
 		if (this.chunk.byteLength > 0) {
-			if (this.asciis.includes(this.chunk[this.index])) {
-				this.parse();
+			if (this.isLack) {
+				this.index = 0;
+				console.log("!!!");
+				return;
+			}
+			if (this.chunk[this.index] === undefined) {
+				this.DEBUG && console.log("目前chunk长度不够，需要等待下次parse调用累积处理:\n" + this._getErrorPositionStr());
+				this.isLack = true;
 			} else {
-				let err = new Error("resp结构有错或者缺失:\n" + this._getErrorPositionStr());
-				this.emit("error", err);
-				throw err;
+				console.log("为毛会来到parse");
+				if (this.asciis.includes(this.chunk[this.index])) {
+					console.log("来到parse了");
+					// this.parse();
+				} else {
+					let err = new Error("resp结构有错, 索引处应该是控制字符 :\n" + this._getErrorPositionStr());
+					this.emit("error", err);
+					throw err;
+				}
 			}
 		}
 	}
